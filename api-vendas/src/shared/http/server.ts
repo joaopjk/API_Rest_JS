72e0ae8f2f96a19cce3bc94ff8368acd796a5e91
joaopjk <joaopjk48@gmail.com>
2021-12-18 14:14:35 -0300

    Configurando o server.ts

--- api-vendas/src/shared/http/server.ts
+++ api-vendas/src/shared/http/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello Dev !");
+import express from "express";
+import cors from 'cors';
+import routes from "./routes";
+
+const app = express();
+app.use(cors()); //Aceitando qualquer origem
+app.use(express.json()) //Configurando a aplicação para interpretar Json
+app.use(routes);
+
+app.listen(3333, () => {
+    console.log("Server started on port 3333!");
+})
