435198bea45c4d43e43b4b2d81538a7d1b1f4541
joaopjk <joaopjk48@gmail.com>
2021-12-20 17:49:42 -0300

    UserAvatarController

---
+++ api-vendas/src/modules/users/controllers/UserAvatarController.ts
@@ -0,0 +1,15 @@
+import { Request, response, Response } from "express";
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
+
+export default class UserAvatarController {
+    public async update(request: Request, reponse: Response): Promise<Response> {
+        const updateAvatar = new UpdateUserAvatarService();
+
+        const user = updateAvatar.execute({
+            user_id: request.user.id,
+            avatarFileName: request.file?.filename as string
+        })
+
+        return response.json(user);
+    }
+}
