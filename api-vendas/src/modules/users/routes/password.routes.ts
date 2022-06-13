0fcd5e02ec4b735f8d9b1930fd64a44c9718c2e6
joaopjk <joaopjk48@gmail.com>
2022-06-13 20:39:02 -0300

    ResetPasswordController

--- api-vendas/src/modules/users/routes/password.routes.ts
+++ api-vendas/src/modules/users/routes/password.routes.ts
@@ -2,9 +2,11 @@ import { celebrate, Segments } from "celebrate";
 import { Router } from "express";
 import Joi from "joi";
 import ForgotPasswordController from "../controllers/ForgotPasswordController";
+import ResetPasswordController from "../controllers/ResetPasswordController";
 
 const passwordRouter = Router();
 const forgotController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRouter.post("/forgot",
     celebrate({
@@ -13,5 +15,15 @@ passwordRouter.post("/forgot",
         }
     }),
     forgotController.create);
+
+passwordRouter.post("/reset",
+    celebrate({
+        [Segments.BODY]: {
+            token: Joi.string().uuid().required(),
+            password: Joi.string().required(),
+            password_confirmation: Joi.string().required().valid(Joi.ref("password"))
+        }
+    }),
+    resetPasswordController.create);
 
 export default passwordRouter;
