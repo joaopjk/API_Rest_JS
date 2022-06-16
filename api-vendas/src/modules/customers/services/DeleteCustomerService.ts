c409d3b1364384994ea72973ac63721cbfac76cd
joaopjk <joaopjk48@gmail.com>
2022-06-16 19:27:23 -0300

    DeleteCustomerService

---
+++ api-vendas/src/modules/customers/services/DeleteCustomerService.ts
@@ -0,0 +1,22 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import Customer from "../typeorm/entities/Customer";
+import CustomerRepository from "../typeorm/repositories/CustomerRepository";
+
+interface IRequest {
+    id: string;
+}
+
+class DeleteCustomerService {
+    public async execute({ id }: IRequest): Promise<void> {
+        const customersRepository = getCustomRepository(CustomerRepository);
+
+        const customer = await customersRepository.findById(id);
+        if (!customer)
+            throw new AppError("Cliente não encontrado!", 404);
+
+        await customersRepository.remove(customer);
+    }
+}
+
+export default DeleteCustomerService;
