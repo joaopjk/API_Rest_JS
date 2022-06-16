1f3036b97ee38662639205ebe783ce15e6de5c9a
joaopjk <joaopjk48@gmail.com>
2022-06-16 17:54:43 -0300

    UpdateProfileService

---
+++ api-vendas/src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,53 @@
+import AppError from "@shared/errors/AppError";
+import { compare, hash } from "bcryptjs"
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+
+interface IRequest {
+    user_id: string;
+    name: string;
+    email: string;
+    password?: string;
+    old_password?: string;
+}
+
+class UpdateProfileService {
+    public async execute({
+        user_id,
+        name,
+        email,
+        password,
+        old_password
+    }: IRequest): Promise<User> {
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const user = await usersRepository.findById(user_id);
+        if (!user)
+            throw new AppError("Usuário não encontrado!", 404);
+
+        const userUpdateEmail = await usersRepository.findByEmail(email);
+        if (userUpdateEmail && userUpdateEmail.id !== user_id)
+            throw new AppError("O email já está sendo utilizado por outro usuário!", 404);
+
+        if (password && !old_password)
+            throw new AppError("O password antigo é requerido", 404);
+
+        if (password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password)
+            if (!checkOldPassword)
+                throw new AppError("O password antigo não deu match!", 404)
+
+            user.password = await hash(password, 8);
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export default UpdateProfileService;
