3b1f52d1b507c5aa8d83849566bf1ded36d429ae
joaopjk <joaopjk48@gmail.com>
2022-06-17 00:08:52 -0300

    Orders routes

--- api-vendas/src/shared/http/routes/index.ts
+++ api-vendas/src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import customersRoutes from "@modules/customers/routes/customer.routes";
+import ordersRoute from "@modules/orders/routes/orders.routes";
 import productsRouter from "@modules/products/routes/products.routes";
 import passwordRouter from "@modules/users/routes/password.routes";
 import profileRouter from "@modules/users/routes/profile.routes";
@@ -13,4 +14,5 @@ routes.use("/sessions", sessionsRouter);
 routes.use("/password", passwordRouter)
 routes.use("/profile", profileRouter)
 routes.use("/customers", customersRoutes)
+routes.use("/orders", ordersRoute);
 export default routes;
