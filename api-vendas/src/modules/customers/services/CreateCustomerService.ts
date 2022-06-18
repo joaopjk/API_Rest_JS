49ccc51c9749f9af92450f5ad529e7850f2c029d
joaopjk <joaopjk48@gmail.com>
2022-06-18 11:01:23 -0300

    Refatorando o módulo de Customers

--- api-vendas/src/modules/customers/services/CreateCustomerService.ts
+++ api-vendas/src/modules/customers/services/CreateCustomerService.ts
@@ -1,16 +1,12 @@
 import AppError from "@shared/errors/AppError";
 import { hash } from "bcryptjs";
 import { getCustomRepository } from "typeorm";
+import { ICreateCustomer } from "../domain/models/ICreateCustomer";
 import Customer from "../infra/typeorm/entities/Customer";
 import CustomerRepository from "../infra/typeorm/repositories/CustomerRepository";
 
-interface IRequest {
-    name: string;
-    email: string;
-}
-
 class CreateCustomerService {
-    public async execute({ name, email }: IRequest): Promise<Customer> {
+    public async execute({ name, email }: ICreateCustomer): Promise<Customer> {
         const customersRepository = getCustomRepository(CustomerRepository);
 
         const customerEmailExists = await customersRepository.findByEmail(email);
